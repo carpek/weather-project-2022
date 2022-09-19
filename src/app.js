9ab9f6ed2020e3deeae4596c1f441b5372c92d22
carpek <77640624+carpek@users.noreply.github.com>
2022-09-19 22:32:50 +0200

    added codesandbox code

--- src/app.js
+++ src/app.js
@@ -1,54 +1,79 @@
+function enterCity(event) {
+  event.preventDefault();
+  let h1 = document.querySelector("#weather-city");
+  let input = document.querySelector("#example-input-city");
+  h1.innerHTML = input.value;
+}
+
 // Date-Display
 let now = new Date();
 
-let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
 let weekDay = days[now.getDay()];
 let day = now.getDate();
-let months = ["Jan.", "Feb.", "Mar.", "Apr.", "May", "Jun.", "Jul.", "Aug.", "Sept.", "Oct.", "Nov.", "Dec."];
+let months = [
+  "Jan.",
+  "Feb.",
+  "Mar.",
+  "Apr.",
+  "May",
+  "Jun.",
+  "Jul.",
+  "Aug.",
+  "Sept.",
+  "Oct.",
+  "Nov.",
+  "Dec."
+];
 let currentMonth = months[now.getMonth()];
 let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
 let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
 let year = now.getFullYear();
 
 let currentDate = document.querySelector("#current-date");
-currentDate.innerHTML = weekDay + ", " + day + "." + currentMonth + " " + year + ", " + hours + "." + minutes;
+currentDate.innerHTML =
+  weekDay +
+  ", " +
+  day +
+  "." +
+  currentMonth +
+  " " +
+  year +
+  ", " +
+  hours +
+  "." +
+  minutes;
 
 let currentDay = document.querySelector("#current-day");
 currentDay.innerHTML = weekDay;
 
-// Search engine api + calls
-
-function search(event) {
-event.preventDefault();
-let apiKey = "bfc01845a820d4f79bd680fec76e9289";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-let city = document.querySelector("#city-search-input").value;
-axios.get(apiUrl).then(showWeather);
-};
-
-
-function showWeather(response) {
-//city api call into html doc
-  document.querySelector("#weather-city").innerHTML = response.data.name;
-  
-// temperature api call into html doc
-  document.querySelector("#current-degrees").innerHTML = Math.round(response.data.main.temp);
-  
-  }
-
-// search engine:
 let form = document.querySelector("form");
-form.addEventListener("submit", search);
+form.addEventListener("submit", enterCity);
 
+// Celcius - Fahrenheit Calculation
 
- 
+function fahrenheitCalculation(event) {
+  event.preventDefault();
+  let currentTemperature = document.querySelector("#current-degrees");
+  let temperature = currentTemperature.innerHTML;
+  temperature = Number(temperature);
+  currentTemperature.innerHTML = Math.round((temperature * 9) / 5 + 32);
+}
 
-
-
-
-
-
-
-
-
-
+let fahrenheitTemperature = document.querySelector("#fahrenheit-link");
+fahrenheitTemperature.addEventListener("click", fahrenheitCalculation);
