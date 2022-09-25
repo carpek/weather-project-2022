76faf0fea91434589ee5886ad1a146dd72c189ef
carpek <77640624+carpek@users.noreply.github.com>
2022-09-25 15:30:10 +0200

    worked on icons

--- src/app.js
+++ src/app.js
@@ -5,12 +5,29 @@ function enterCity(event) {
   h1.innerHTML = input.value;
 }
 
+function showTemperature(response) {
+
+
+}
+
+function showPosition(position) {
+let lat = position.coord.lat;
+let lon = position.coord.lon;
+
 let apiKey = "bfc01845a820d4f79bd680fec76e9289";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Vienna&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(showTemperature);
+}
+
+navigator.geolocation.getCurrentPosition(showPosition);
 
 //Weather
 
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+let city = "Vienna";
+
 function showWeather(response) {
+
 let city = document.querySelector("#weather-city");
 city.innerHTML = response.data.name;
 
@@ -20,20 +37,42 @@ temperatureElement.innerHTML = Math.round(response.data.main.temp);
 let weatherElement = document.querySelector("#weatherToday");
 weatherElement.innerHTML = response.data.weather[0].description;
 
+let iconElement = document.querySelector("#iconToday");
+let iconArray = response.data.weather[0].icon;
+iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${iconArray}@2x.png`);
+
 let humidityElement = document.querySelector("#humidity");
 humidityElement.innerHTML = response.data.main.humidity;
 
 let windElement = document.querySelector("#windSpeed");
-windElement = response.data.wind.speed;
+windElement.innerHTML = Math.round(response.data.wind.speed);
 
 let realTempElement = document.querySelector("#realFeel");
-realTempElement = response.data.main.feels_like;
+realTempElement.innerHTML = Math.round(response.data.main.feels_like);
+
+let sunriseElement = document.querySelector("sunrise");
+sunriseElement.innerHTML = response.data.sys.sunrise * 1000;
+
+let sunsetElement = document.querySelector("sunset");
+sunsetElement.innerHTML = response.data.sys.sunset * 1000;
+
+let celsiusTemperature = document.querySelector("#current-degrees");
+celsiusTemperature.innerHTML = response.data.main.temp;
+  
+let minCelsius = document.querySelector("#min-temp");
+minCelsius.innerHTML = Math.round(response.data.main.temp_min);
+
+let maxCelsius = document.querySelector("#max-temp");
+maxCelsius.innerHTML = Math.round(response.data.main.temp_max);
 
 }
   
   axios.get(apiUrl).then(showWeather);
 // Date-Display
-let now = new Date();
+
+function formateDate(timestamp)  {
+  
+let now = new Date(timestamp);
 
 let days = [
   "Sunday",
@@ -55,7 +94,7 @@ let months = [
   "Jun.",
   "Jul.",
   "Aug.",
-  "Sept.",
+  "Sep.",
   "Oct.",
   "Nov.",
   "Dec."
@@ -85,6 +124,8 @@ currentDate.innerHTML =
   hours +
   "." +
   minutes;
+
+}
 
 let form = document.querySelector("form");
 form.addEventListener("submit", enterCity);
